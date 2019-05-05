ff3bfcfc8b1a538f2c34dc4c63106b4b7e0555f4
ashimagg <ashim.agg93@gmail.com>
2019-05-05 11:51:01 -0400

    rekognition service

--- lambda/UserDashboard/dynamo_service.py
+++ lambda/UserDashboard/dynamo_service.py
@@ -1,20 +1,21 @@
 import boto3
 import time
 
-device_owner_id_field = 'owner_id'
-face_id_field = 'face_id'
+# device_owner_id_field = 
+# face_id_field = 
 
 def get_user_id_for_image(face_id, device_owner_id):
+    print("Retrieve user id for face_id[" + face_id + "] and owner[" + device_owner_id + "]")
     dynamodb = boto3.resource("dynamodb", region_name='us-east-1')
     table = dynamodb.Table('images_cc_proj')    
     try:
         response = table.get_item(
             Key={
-                face_id_field : face_id,
-                device_owner_id_field : device_owner_id,
+                'face_id' : face_id,
             }
         )
     except Exception as e:
+        print("Error in retrieving user id from face id")
         print(e.response['Error']['Message'])
         return None
     else :
@@ -22,30 +23,35 @@ def get_user_id_for_image(face_id, device_owner_id):
             item = response['Item']
             print ("Returning user id : {} for input face_id id : {}".format(item['user_id'], face_id))
             return item['user_id']
+    print("No Users Found")
     return None
     
 def put_face_record(face_id, user_id, path, device_owner_id):
+    
+    print("Inserting Face Record[" + str(face_id) + "] for user[" + str(user_id) + "], owner[" + str(device_owner_id) + "]")
     dynamodb = boto3.resource("dynamodb", region_name='us-east-1')
     table = dynamodb.Table('images_cc_proj')    
     response = table.put_item(
         Item={
-            face_id_field : face_id,
+            'face_id' : face_id,
             'user_id': user_id,
             's3_path' : path,
-            device_owner_id_field : device_owner_id,
+            'owner_id' : device_owner_id,
         }
     )
-    print("Put Face Record succeeded:")
+    print("Insertion for Face Record Done")
     
 def put_user_record(user_id, device_owner_id):
+    
+    print("Inserting User Record[" + user_id + "] for owner[" + device_owner_id + "]")
     dynamodb = boto3.resource("dynamodb", region_name='us-east-1')
     table = dynamodb.Table('users_cc_proj')    
     response = table.put_item(
         Item={
             'user_id': user_id,
             'tagged' : False,
-            device_owner_id_field : device_owner_id,
+            'owner_id' : device_owner_id,
         }
     )
-    print("Put User Record succeeded:")
+    print("Insertion for User Record Done")
     
