5e72e2d5c2990c34846299298713963901ae331b
ashimagg <ashim.agg93@gmail.com>
2019-05-09 08:59:08 -0400

    refactor rds_service

--- lambda/UserDashboard/rds_service.js
+++ lambda/UserDashboard/rds_service.js
@@ -1,13 +1,10 @@
 'use strict';
 
-const https = require('https');
-const AWS = require('aws-sdk')
+const AWS = require('aws-sdk');
+const mysql = require('mysql');
+const config = require('./config.json');
 
-
-let mysql = require('mysql');
-let config = require('./config.json');
-
-let pool = mysql.createPool({
+const pool = mysql.createPool({
     host: config.dbhost,
     user: config.dbuser,
     password: config.dbpassword,
@@ -27,7 +24,26 @@ module.exports.getSurveillanceImages = async function (from_date, to_date, user_
             }    
             else{
                 console.log("Successfully connected to db");
-                let sql = `select * from admin.images_cc_proj a inner join admin.users_cc_proj b 
+                let sql_query = build_sql_query(from_date, to_date, user_name);
+                connection.query(sql_query, function (error, results, fields) {
+                    connection.release();
+                    if (error) {
+                        console.log("Error in fetching query results -" + error);
+                        reject(error);
+                    }
+                    else {
+                        console.log(`Successfully fetched ${results.length} results from db`);
+                        let response_json = build_response(results);
+                        resolve(response_json);
+                    }
+                });
+            }
+        });
+    });
+};
+
+function build_sql_query(from_date, to_date, user_name){
+    let sql = `select * from admin.images_cc_proj a inner join admin.users_cc_proj b 
                 ON a.user_id = b.user_id`;
                 if (from_date) {
                     sql += ` and inserted_time > ${from_date}`;
@@ -38,45 +54,28 @@ module.exports.getSurveillanceImages = async function (from_date, to_date, user_
                 if (user_name){
                     sql += ` or user_name = "${user_name}"`; 
                 }
-                sql += `order by inserted_time`;
-                // console.log(sql);
-                connection.query(sql, function (error, results, fields) {
-                    connection.release();
-                    if (error) {
-                        console.log("Error in fetching query results -" + error);
-                        reject(error);
-                    }
-                    else {
-                        console.log(`Successfully fetched ${results.length} results from db`);
-                        // console.log(results);
-                        let json = {};
-                        results.forEach(function(item) {
-                            let s3_path = item.s3_path;
-                            let bounding_box = item.bounding_box;
-                            bounding_box = bounding_box.split(",");
-                            let width = bounding_box[0];
-                            let height = bounding_box[1];
-                            let left = bounding_box[2];
-                            let top = bounding_box[3];
-                            let user_name = item.user_name;
-                            let user_id = item.user_id;
-                            let timestamp = item.inserted_time;
-                            let tagged = item.tagged;
-                            let userDetail = {"userId" : user_id, "tagged" : tagged, "width" : width,
-                                'height' : height, 'left' : left, 'top' : top, 'userName' : user_name};
-                            if (!(s3_path in json)){
-                                json[s3_path] = {'timestamp' : timestamp ,'bounding_box' :[] };
-                            }
-                            let userDetails = json[s3_path].bounding_box;
-                            userDetails.push(userDetail); 
-                            
-                        });
-                        
-                        resolve(json);
-                    }
-                });
-            }
-        });
-    });
-};
+                sql += `order by inserted_time`; 
+    return sql;
+}
 
+function build_response(results){
+    let response_json = {};
+    results.forEach(function(item) {
+        let s3_path = item.s3_path;
+        let bounding_box = item.bounding_box.split(",");
+        let userDetail = {"userId" : item.user_id, 
+                        "tagged" : item.tagged,
+                        "width" : bounding_box[0],
+                        "height" : bounding_box[1], 
+                        "left" : bounding_box[2], 
+                        "top" : bounding_box[3], 
+                        "userName" : item.user_name,
+                    };
+        if (!(s3_path in response_json)){
+            response_json[s3_path] = {'timestamp' : item.inserted_time ,'bounding_box' :[] };
+        }
+        let userDetails = response_json[s3_path].bounding_box;
+        userDetails.push(userDetail); 
+    });
+    return response_json;
+}
